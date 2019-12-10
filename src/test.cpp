68d145204eb81b408db33f8230177626fa110098
yangdianxp <41214656+yangdianxp@users.noreply.github.com>
2019-12-10 21:55:50 +0800

    ...

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <type_traits>
 
+template <size_t... values>
+constexpr size_t fun()
+{
+	return (0 + ... + values);
+}
+
+constexpr size_t res = fun<1, 2, 3, 4, 5>();
 
 int main()
 {
-
-
+	std::cout << res << std::endl;
 }
