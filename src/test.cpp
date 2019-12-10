ffcb3d2c71c887ef04cd542c4147ac64c26ea40c
yangdianxp <yangdianxp@163.com>
2019-12-10 19:43:13 +0800

    ...

--- src/test.cpp
+++ src/test.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <type_traits>
 
-template <int... Vals> struct IntContainer;
-template <bool... Vals> struct BoolContainer;
-template <typename... Types> struct TypeContainer;
-template <template <typename> class...T> struct TemplateCont;
-template <template <typename...> class...T> struct TemplateCont2;
-
 
 int main()
 {
+
 
 }
